6f2554368980c1459fb60f899cedab4bb5920c8b
ryane <ryanesc@gmail.com>
2021-12-23 16:45:05 -0500

    configmap injector will generate target configmap
    if it doesn't exist.
    
    + more tests

--- configmap-injector/configmapinjector/configmapinjector.go
+++ configmap-injector/configmapinjector/configmapinjector.go
@@ -1,6 +1,7 @@
 package configmapinjector
 
 import (
+	"sigs.k8s.io/kustomize/api/konfig"
 	"sigs.k8s.io/kustomize/kyaml/fn/framework"
 	"sigs.k8s.io/kustomize/kyaml/yaml"
 )
@@ -40,9 +41,15 @@ func (i *ConfigMapInjector) Filter(items []*yaml.RNode) ([]*yaml.RNode, error) {
 		).Match
 	}
 
+	injectMap := map[*yaml.RNode]bool{}
+	for _, inject := range injects {
+		injectMap[inject] = false
+	}
+
 	for i, item := range items {
-		for _, inject := range injects {
+		for inject := range injectMap {
 			if isInjectTarget(inject)(item) {
+				injectMap[inject] = true
 				data, err := inject.GetFieldValue("data")
 				if err != nil {
 					return items, err
@@ -70,6 +77,48 @@ func (i *ConfigMapInjector) Filter(items []*yaml.RNode) ([]*yaml.RNode, error) {
 				configMap.SetDataMap(cmdata)
 				items[i] = configMap
 			}
+		}
+	}
+
+	for inject, injected := range injectMap {
+		if !injected {
+			const cmTemplate = `apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: cm
+`
+			configMap, err := yaml.Parse(cmTemplate)
+			if err != nil {
+				return items, err
+			}
+			configMap.SetName(inject.GetName())
+			configMap.SetNamespace(inject.GetNamespace())
+			configMap.SetLabels(inject.GetLabels())
+
+			annotations := inject.GetAnnotations()
+			delete(annotations, konfig.IgnoredByKustomizeAnnotation)
+			configMap.SetAnnotations(annotations)
+
+			data, err := inject.GetFieldValue("data")
+			if err != nil {
+				return items, err
+			}
+			dataMap, ok := data.(map[string]interface{})
+			if !ok {
+				// TODO: how do we surface the fact that we can't continue with the
+				// injection?
+				continue
+			}
+			transformed := map[string]string{}
+			for key, val := range dataMap {
+				yml, err := yaml.Marshal(val)
+				if err != nil {
+					return items, err
+				}
+				transformed[key] = string(yml)
+			}
+			configMap.SetDataMap(transformed)
+			items = append(items, configMap)
 		}
 	}
 
