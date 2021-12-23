6fd5f01923795a7847ef63f3f8a143b38d4ad77a
ryane <ryanesc@gmail.com>
2021-12-23 17:00:36 -0500

    cleanup / refactor

--- configmap-injector/configmapinjector/configmapinjector.go
+++ configmap-injector/configmapinjector/configmapinjector.go
@@ -1,6 +1,8 @@
 package configmapinjector
 
 import (
+	"fmt"
+
 	"sigs.k8s.io/kustomize/api/konfig"
 	"sigs.k8s.io/kustomize/kyaml/fn/framework"
 	"sigs.k8s.io/kustomize/kyaml/yaml"
@@ -11,6 +13,12 @@ const (
 	apiVersionConfigMap = "v1"
 	kindInject          = "ConfigMapInject"
 	kindConfigMap       = "ConfigMap"
+
+	configMapTemplate = `apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: cm
+`
 )
 
 type ConfigMapInjector struct{}
@@ -24,6 +32,10 @@ func (i *ConfigMapInjector) Filter(items []*yaml.RNode) ([]*yaml.RNode, error) {
 	injects, err := injectSelector.Filter(items)
 	if err != nil {
 		return items, err
+	}
+	injectMap := map[*yaml.RNode]bool{}
+	for _, inject := range injects {
+		injectMap[inject] = false
 	}
 
 	isConfigMap := framework.ResourceMatcherFunc(func(node *yaml.RNode) bool {
@@ -41,86 +53,80 @@ func (i *ConfigMapInjector) Filter(items []*yaml.RNode) ([]*yaml.RNode, error) {
 		).Match
 	}
 
-	injectMap := map[*yaml.RNode]bool{}
-	for _, inject := range injects {
-		injectMap[inject] = false
-	}
-
+	// look for target configmaps and inject data
 	for i, item := range items {
 		for inject := range injectMap {
 			if isInjectTarget(inject)(item) {
 				injectMap[inject] = true
-				data, err := inject.GetFieldValue("data")
+				configMap, err := injectConfigMap(inject, item.Copy())
 				if err != nil {
 					return items, err
 				}
-				dataMap, ok := data.(map[string]interface{})
-				if !ok {
-					// TODO: how do we surface the fact that we can't continue with the
-					// injection?
-					continue
-				}
-				transformed := map[string]string{}
-				for key, val := range dataMap {
-					yml, err := yaml.Marshal(val)
-					if err != nil {
-						return items, err
-					}
-					transformed[key] = string(yml)
-				}
-
-				configMap := item.Copy()
-				cmdata := configMap.GetDataMap()
-				for key, val := range transformed {
-					cmdata[key] = val
-				}
-				configMap.SetDataMap(cmdata)
 				items[i] = configMap
 			}
 		}
 	}
 
+	// if no injection occurred, generate a new configmap
 	for inject, injected := range injectMap {
 		if !injected {
-			const cmTemplate = `apiVersion: v1
-kind: ConfigMap
-metadata:
-  name: cm
-`
-			configMap, err := yaml.Parse(cmTemplate)
+			configMap, err := newConfigMap(inject)
 			if err != nil {
 				return items, err
 			}
-			configMap.SetName(inject.GetName())
-			configMap.SetNamespace(inject.GetNamespace())
-			configMap.SetLabels(inject.GetLabels())
-
-			annotations := inject.GetAnnotations()
-			delete(annotations, konfig.IgnoredByKustomizeAnnotation)
-			configMap.SetAnnotations(annotations)
-
-			data, err := inject.GetFieldValue("data")
+			configMap, err = injectConfigMap(inject, configMap)
 			if err != nil {
 				return items, err
 			}
-			dataMap, ok := data.(map[string]interface{})
-			if !ok {
-				// TODO: how do we surface the fact that we can't continue with the
-				// injection?
-				continue
-			}
-			transformed := map[string]string{}
-			for key, val := range dataMap {
-				yml, err := yaml.Marshal(val)
-				if err != nil {
-					return items, err
-				}
-				transformed[key] = string(yml)
-			}
-			configMap.SetDataMap(transformed)
+
 			items = append(items, configMap)
 		}
 	}
 
 	return items, nil
+}
+
+func newConfigMap(inject *yaml.RNode) (*yaml.RNode, error) {
+	configMap, err := yaml.Parse(configMapTemplate)
+	if err != nil {
+		return nil, err
+	}
+	configMap.SetName(inject.GetName())
+	configMap.SetNamespace(inject.GetNamespace())
+	configMap.SetLabels(inject.GetLabels())
+
+	annotations := inject.GetAnnotations()
+	delete(annotations, konfig.IgnoredByKustomizeAnnotation)
+	configMap.SetAnnotations(annotations)
+
+	return configMap, nil
+}
+
+func injectConfigMap(inject *yaml.RNode, configMap *yaml.RNode) (*yaml.RNode, error) {
+	data, err := inject.GetFieldValue("data")
+	if err != nil {
+		return configMap, err
+	}
+	dataMap, ok := data.(map[string]interface{})
+	if !ok {
+		return configMap, fmt.Errorf(
+			"data must be a map[string]interface, got %T",
+			data,
+		)
+	}
+	transformed := map[string]string{}
+	for key, val := range dataMap {
+		yml, err := yaml.Marshal(val)
+		if err != nil {
+			return configMap, err
+		}
+		transformed[key] = string(yml)
+	}
+
+	cmdata := configMap.GetDataMap()
+	for key, val := range transformed {
+		cmdata[key] = val
+	}
+	configMap.SetDataMap(cmdata)
+	return configMap, nil
 }
